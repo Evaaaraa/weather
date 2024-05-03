2dd8909808c5bbe4055e55ac4a4468edeeaa4559
Evaaaraa <jenifferjojo0000@gmail>
2024-05-03 15:12:07 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,10 @@ function App() {
   const [search,setSearch] = useState("")
   const [data,setData] = useState({})
   const clouds = [
-    {weather :"few clouds",image :"./few.jpeg"},
-    {weather :"scattered clouds",image :"./scattered.jpeg"},
-    {weather :"broken clouds",image :"./broken.jpeg"},
-    {weather :"overcast clouds",image :"./overcast.jpg"},
+    {weather :"few clouds",image :"./few.gif"},
+    {weather :"scattered clouds",image :"./scattered.webp"},
+    {weather :"broken clouds",image :"./broken.gif"},
+    {weather :"overcast clouds",image :"./overcast.gif"},
   ]
 
   async function getWeather(){
@@ -23,29 +23,34 @@ function App() {
     alert("Enter Location")
    }
   }
+  async function getKochi(){
+    const kochiRes = await axios.get('https://api.openweathermap.org/data/2.5/weather?q=kochi&appid=e88cdb46830b14b1806bf31a738563d6')
+    setData(kochiRes.data)
+  }
   useEffect(()=>{
-    async function getKochi(){
-      const kochiRes = await axios.get('https://api.openweathermap.org/data/2.5/weather?q=kochi&appid=e88cdb46830b14b1806bf31a738563d6')
-      setData(kochiRes.data)
-    }
     getKochi()
+    // clouds.map((item)=>{item.weather=="broken clouds"?console.log(item.weather):console.log("empty");})
   },[])
+  // console.log(` dat is${data.weather&&data.weather[0].description}`);
+  //console.log(clouds.find((item)=>item.weather===(data.weather&&data.weather[0].description))?.image);
+  
   // console.log(data);
   return (
   <>
-      <div className="container-fluid position-relative" >
+      <div className="container-fluid" >
 
         
-        <section className="vh-100" style={{backgroundColor: "#fffff0"}}>
-          <div className="container-fluid py-5 h-100">
+      <section className="vh-100" style={{backgroundImage: data? `url(${clouds.find((item)=>item.weather===(data.weather&&data.weather[0].description))?.image})`:"",backgroundSize : 'cover',backgroundPosition :'center',filter :'brightness(100%)'}}>
+    
+          <div className="container py-5 h-100">
             
-            <div className="row d-flex h-100">
+            <div className="row d-flex justify-content-center  h-100">
               
               <div className="col-md-10 col-lg-8 col-xl-6">
             
                 <div className="container-fluid m-3">
-                  <div className="d-flex" role="search">
-                    <input className="form-control me-2" id='input' onChange={(e)=>{setSearch(e.target.value)}} type="search" placeholder="Search" aria-label="Search"/>
+                  <div className="d-flex opacity-75" role="search">
+                    <input className="form-control me-2" id='input' onChange={(e)=>{setSearch(e.target.value)}} type="search" placeholder="Search"  aria-label="Search"/>
                     <button className="btn btn-outline-primary" onClick={getWeather} type="submit">Search</button>
                   </div>
                 </div>
@@ -53,12 +58,6 @@ function App() {
                 <div className="card bg-light text-dark" style={{borderRadius: "40px"}}>
                   <div className="bg-image position-relative" style={{borderRadius: "35px"}}>
                     <div className=" podition-absolute top-0 start-0" >
-                    {
-                      clouds.map((e,index)=>(
-                        e.weather==(data.weather && data.weather[0].description)?<img src={e.image}
-                        className="card-img" key={index} style={{width: "100vw",height: "100vh",objectFit : "cover",borderRadius: "35px"}} alt="weather"/>:<></>
-                      ))
-                    }
                     </div>
                     <div className="mask" style={{backgroundColor: "rgba(190, 216, 232, .5)"}}></div>
                   </div>
